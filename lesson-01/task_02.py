470a2e996456041d38cd51577268af3ea3a0f700
Andrey Pashkin <pashkinan@cbr.ru>
2021-07-26 00:14:10 +0300

    Поправки по замечаниям для заданий к уроку 1

--- lesson-01/task_02.py
+++ lesson-01/task_02.py
@@ -2,11 +2,11 @@
 # Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс. Используйте форматирование строк.
 
 # Вариант 1
-timeInSeconds = input('Введите время в секундах:')
+timeInSeconds = int(input('Введите время в секундах:'))
 
-hours = int(timeInSeconds) // 3600
-minutes = (int(timeInSeconds) - 3600 * (int(timeInSeconds) // 3600)) // 60
-seconds = int(timeInSeconds) % 60
+hours = timeInSeconds // 3600
+minutes = (timeInSeconds - 3600 * (timeInSeconds // 3600)) // 60
+seconds = timeInSeconds % 60
 
 if hours < 10:
     hours = '0' + str(hours)
@@ -20,10 +20,10 @@ if seconds < 10:
 print(f'{hours}:{minutes}:{seconds}')
 
 # Вариант 2: С использованием спецификаторов формата 'd' и '%':
-timeInSeconds = input('Введите ещё раз время в секундах:')
+timeInSeconds = int(input('Введите ещё раз время в секундах:'))
 
-hours = int(timeInSeconds) // 3600
-minutes = (int(timeInSeconds) - 3600 * (int(timeInSeconds) // 3600)) // 60
-seconds = int(timeInSeconds) % 60
+hours = timeInSeconds // 3600
+minutes = (timeInSeconds - 3600 * (timeInSeconds // 3600)) // 60
+seconds = timeInSeconds % 60
 
 print('%02d:%02d:%02d' % (hours, minutes, seconds))
